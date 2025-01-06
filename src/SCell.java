54a54bd39ec0c83ab96ba1dc3bb2a1e5f2338975
danielbaranetz <dbaranetz@gmail.com>
2025-01-06 23:35:26 +0200

    v0.0.3

--- src/SCell.java
+++ src/SCell.java
@@ -31,13 +31,9 @@ public class SCell implements Cell {
             type = Ex2Utils.TEXT;
         } else if (Ex2.isNumber(s)) {
             type = Ex2Utils.NUMBER;  // If it's a number, set type to NUMBER
-//            line = Double.valueOf(s).toString();
         } else if (Ex2.isForm(s)) {
-//            line = Ex2.computeForm(s).toString();
-            // If it's a valid formula, set type to FORM
             type = Ex2Utils.FORM;
         } else {
-//            line = Ex2Utils.ERR_FORM;
             type = Ex2Utils.ERR_FORM_FORMAT;
         }
     }
